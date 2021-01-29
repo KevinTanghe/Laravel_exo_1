867b3e0030b29e38e6a55cc86fb6b92f58b1562d
Tanghe Kevin <tanghekevin1@gmail.com>
2021-01-29 15:46:37 +0100

    création des pages, controller et de le chemin dans la page web.php

---
+++ app/Http/Controllers/KevinController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class KevinController extends Controller
+{
+    public function index() {
+        return view('welcome');
+    }
+
+    public function about() {
+        return view('pages/about');
+    }
+
+    public function product() {
+        return view('pages/product');
+    }
+
+    public function store() {
+        return view('pages/store');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\KevinController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,7 +13,10 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/', [KevinController::class, 'index']);
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/about', [KevinController::class, 'about']);
+
+Route::get('/product', [KevinController::class, 'product']);
+
+Route::get('/store', [KevinController::class, 'store']);
